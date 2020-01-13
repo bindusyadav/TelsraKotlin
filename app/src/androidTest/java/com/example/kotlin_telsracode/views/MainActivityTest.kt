5094185d6451b49a0aabc5201a7643f3850f6c15
bindushree <bindushree.s01@infosys.com>
2020-01-13 15:50:54 +0530

    The latest code updated :
    1.Implemented Swipe-to-refresh to the screen, removed button click.
    2.Removed redundant/unused code
    3.Network handling test case written in both instrumentation testcase and Junit.

--- app/src/androidTest/java/com/example/kotlin_telsracode/views/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/kotlin_telsracode/views/MainActivityTest.kt
@@ -36,24 +36,5 @@ class MainActivityTest {
                 click()))
     }
 
-
-    fun networkCondition(): Boolean {
-        var status = false
-        try {
-            val cm =
-                mainActivity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-            var netInfo = cm!!.getNetworkInfo(0)
-            if (netInfo != null && netInfo.state == NetworkInfo.State.CONNECTED) {
-                status = true
-            } else {
-                netInfo = cm.getNetworkInfo(1)
-                if (netInfo != null && netInfo.state == NetworkInfo.State.CONNECTED) status =
-                    true
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return false
-        }
-        return status
-    }
+    
 }
